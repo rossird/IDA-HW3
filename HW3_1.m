1ea0d342e536e19516e98a54b1c504a6a2fcb050
= <=>
2016-11-06 21:43:48 -0500

    Did more of part 1

--- HW3_1.m
+++ HW3_1.m
@@ -1,4 +1,8 @@
-clear all; clc;
+%% K-Means clustering
+clear all; clc; close all;
+
+% We will be using the Squared Euclidean method for determining distance
+% between two points.
 
 % Load data. Only read first 50 entries.
 % Exclude first column, it is student number.
@@ -8,24 +12,36 @@ startK = 3;
 endK = 8;
 kmeansIter = 3;
 
+% Init minimum SSE for each value of k
+minSSE = ones(endK,1) * 10^10;
+clustering = cell(8,2);
+
 % Run k-means for values 3 through 8
 for k = startK : endK
+    fprintf('Running k-means with k = %i\n',k);
     % For each k-means, run three times and choose the clustering with the
     % smallest SSE value.
     for c = 1:kmeansIter
-        fprintf('Running k-means with k = %i\n',k);
         temp = randperm(50);
         seeds = temp(1:k);
         
         % K-means by default uses Squared Euclidean distance.
-        [clusterID, centroids, SUMD, D] = kmeans(Data, k, 'Start', Data(seeds,:));
+        [clusterID, centroids, ~, pointClusterDistance] = kmeans(Data, k, 'Start', Data(seeds,:));
         
         % Find SSE
         SSE = 0;
         for i = 1:50
            cluster = clusterID(i);
-           SSE = SSE + D(i, cluster); 
+           SSE = SSE + pointClusterDistance(i, cluster).^2; 
         end
-        fprintf('SSE: %0.4f\n',SSE);
+        if SSE < minSSE(k)
+           minSSE(k) = SSE;
+           clustering{k} = {centroids clusterID};
+        end
     end
+    fprintf('Running k-means with k = %i\n', minSSE(k));
 end
+plot(3:8, minSSE(3:8))
+title('SSE vs k value')
+xlabel('k value')
+ylabel('Min SSE')
